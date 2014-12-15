e793e05cee396e900ab850c10c4b83b4e46b23e0
David Menendez <damenend@gmail.com>
2014-12-15 13:08:48 -0600

    added some images, mobile nav, and style edits

--- includes/footer.php
+++ includes/footer.php
@@ -2,14 +2,14 @@
             <div class="container">
                 <nav id="footerNav">
                     <ul>
-                        <li><h2><a href="portfolio.html">WORK</a></h2></li>
-                        <li><h2><a href="about.html#resumeContainer">RESUME</a></h2></li>
-                        <li><h2><a href="#">CONTACT</a></h2></li>
+                        <li><h2><a href="portfolio.php">WORK</a></h2></li>
+                        <li><h2><a href="about.php#resumeContainer">RESUME</a></h2></li>
+                        <li><h2><a href="about.php#contactContainer">CONTACT</a></h2></li>
                     </ul>
                 </nav>
             </div>
         </footer>
-        <script src="assets/js/libs/jquery/jquery.js"></script>
+        <script src="assets/js/libs/jquery/jquery-1.11.1.min.js"></script>
         <script src="assets/js/libs/fancybox/jquery.fancybox.js"></script>
         <script src="assets/js/libs/fancybox/jquery.mousewheel-3.0.6.pack.js"></script>
         <script src="assets/js/functions.js"></script>
--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,6 @@
 <section id="siteHeader" class="container">
     <h1>David Menendez</h1>
+    <img id="navicon" src="assets/img/menu.svg" alt="menu icon">
     <nav>
         <ul>
             <li <?php if(basename($_SERVER['PHP_SELF']) == "index.php"){echo "class='active'";}?>><a href="index.php">HOME</a></li>
