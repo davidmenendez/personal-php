719c482a93755e2fb698fcb0596fd423337bf2f6
David Menendez <damenend@gmail.com>
2014-12-31 15:55:29 -0600

    comming soon

---
+++ comingsoon.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo (isset($title)) ? $title . " | David Menendez | Front-End Developer" : "David Menendez | Front-End Developer"; ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400|Open+Sans:400,300|Oswald' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="assets/css/jquery.fancybox.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    </head>
+    <body>
+        <section id="top" class="content construction">
+            <header>
+                <hgroup>
+                    <h1 id="branding">David Menendez</h1>
+                    <h2>Front End Web Developer</h2>
+                    <h1>COMING SOON</h1>
+                </hgroup>
+                <nav id="mainNav">
+                    <ul>
+                        <li><a href="files/david_menendez_resume.pdf">RESUME</a></li>
+                        <li><a href="https://www.linkedin.com/in/davidamenendez" target="_blank">LINKEDIN</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </section>
+        <script src="assets/js/libs/jquery/jquery-1.11.1.min.js"></script>
+        <script src="assets/js/libs/fancybox/jquery.fancybox.min.js"></script>
+        <script src="assets/js/libs/fancybox/jquery.mousewheel-3.0.6.pack.js"></script>
+        <script src="assets/js/functions.js"></script>
+    </body>
+</html>
