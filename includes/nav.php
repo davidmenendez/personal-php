66a62f221a48661c59e805bdec0949752dfaba2a
davidmenendez <damenend@gmail.com>
2015-02-10 22:36:41 -0600

    localhost index redirect

--- includes/nav.php
+++ includes/nav.php
@@ -5,7 +5,6 @@
             <img id="navicon" src="assets/img/menu.svg" alt="menu icon">
             <nav>
                 <ul>
-                    <li <?php if(basename($_SERVER['PHP_SELF']) == "index.php"){echo "class='active'";}?>><a href="<?php echo ($_SERVER['SERVER_NAME'] == "localhost") ? "index.php" : "/" ?>">HOME</a></li>
                     <li <?php if(basename($_SERVER['PHP_SELF']) != "index.php" && basename($_SERVER['PHP_SELF']) != "about.php"){echo "class='active'";}?>><a href="portfolio.php">PORTFOLIO</a></li>
                     <li <?php if(basename($_SERVER['PHP_SELF']) == "about.php"){echo "class='active'";}?>><a href="about.php">ABOUT</a></li>
                     <li><a href="about.php#resumeContainer">RESUME</a></li>
